e58b9db0f8fdc413ab048768d8bc779d81b031da
freedom1810 <nguyenhaison181097@gmail.com>
2021-08-07 20:26:24 +0700

    add preprocess and audio transform to dataset

--- data_loader/dataset.py
+++ data_loader/dataset.py
@@ -6,19 +6,32 @@ import soundfile as sf
 from PIL import Image
 import numpy as np
 import cv2
+import librosa
 from .audio_preprocessing import mfcc_feature, extract_mfcc_feature
-from .mel_spec import audio2image, create_spectrogram
+# from .mel_spec import audio2melspec, create_spectrogram
+from .audio_preprocessing import trim_and_pad, padding_repeat, random_crop
 
 class CovidDataset(torch.utils.data.Dataset):
-    def __init__(self, df, audio_folder, mfcc_config, audio_transforms=None, image_transform=None):
+    def __init__(self, df, audio_folder, config, test = False, audio_transforms=None, image_transform=None):
         super().__init__()
         self.df = df
         self.audio_folder = audio_folder
         self.image_transform = image_transform
         self.audio_transforms = audio_transforms
-        self.mfcc_config = mfcc_config
-        if mfcc_config is None:
+        self.test = test
+
+        self.mfcc_config = config['dataset'].get('mfcc_config', None)
+        if self.mfcc_config is None:
             self.mfcc_config = {}
+        
+        self.target_sr = self.mfcc_config.get('target_sr', 48000)
+        max_duration = self.mfcc_config.get('max_duration', 15)
+        self.max_samples = int(max_duration * self.target_sr)
+
+        # self.melspec_config = config['dataset']['melspec_config']
+        # target_sr = self.melspec_config.get('target_sr', 48000)
+        # max_duration = self.melspec_config.get('max_duration', 15)
+        # self.max_samples = int(max_duration * target_sr)
 
     def get_label(self, idx):
         return self.df.iloc[idx]["assessment_result"].astype("float32")
@@ -29,20 +42,39 @@ class CovidDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         item = self.df.iloc[idx]
         label = item["assessment_result"].astype("float32")
-        #print(label)
+        # one hot
         # label_encoded = np.zeros((label.size, 2))
         # label_encoded[np.arange(label.size),int(label)] = 1
 
-        label_encoded = torch.tensor(label, dtype=torch.float)
+        label_encoded = torch.tensor(label, dtype=torch.long)
 
-        audio_path = os.path.join(self.audio_folder, "%s.wav"%item['uuid'])
-        audio, fs = sf.read(audio_path, dtype="float32")
+        try:
+            audio_path = os.path.join(self.audio_folder, "%s.wav"%item['uuid'])
+            audio, sr = sf.read(audio_path, dtype="float32")
+        except:
+            audio_path = item['audio_paths']
+            audio, sr = sf.read(audio_path, dtype="float32")
+        audio = librosa.resample(audio, sr, self.target_sr)
+        # audio, sr = librosa.load(audio_path, sr = self.target_sr)
+        
+        if self.test:
+            audio = padding_repeat(audio, self.max_samples)
+        else:
+            audio = random_crop(audio, self.max_samples)
+            # audio = trim_and_pad(audio, self.max_samples)
+
+        if self.audio_transforms is not None:
+            audio = self.audio_transforms(samples=audio, sample_rate=sr)
+        #mfcc
         # # image = audio2image(audio, fs, self.audio_transforms)
         # image = mfcc_feature(audio, fs, self.audio_transforms)
-        image = extract_mfcc_feature(audio, fs, self.mfcc_config, self.audio_transforms, for_test=False)
-        # image = create_spectrogram(audio, fs, self.audio_transforms)
+        image = extract_mfcc_feature(audio, sr, self.mfcc_config)
+        
+        # melspec
+        # image = audio2melspec(audio, sr, self.melspec_config)
+
         if self.image_transform is not None:
-            image = self.image_transform(image)
+            image = self.image_transform(image = image)['image']
 
         return image, label_encoded
 
